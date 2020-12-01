60d87d8cdfcbb937ba8d1c82617a707a9edcd1b7
Arman Salehi <arman.salehi@student.rocva.nl>
2020-12-01 14:05:00 +0100

    array gefixt

--- 3-Arrays/taak04 - Multidimensionaal/index.php
+++ 3-Arrays/taak04 - Multidimensionaal/index.php
@@ -1,20 +1,13 @@
 <!DOCTYPE HTML>
 <html>
 <body>
-<ul>
 <?php
-$abcGetallen[0] = ["a"];
-$abcGetallen[1] = ["b"];
-$abcGetallen[2] = ["c"];
-$abcGetallen[3] = ["d"];
-$abcGetallen[4] = "24";
-$abcGetallen[5] = "56";
-$abcGetallen[6] = "78";
-$abcGetallen[7] = "90";
+ 
+$abcGetallen = [["a","b","c","d"],24, 56, 78, 90];
 print_r($abcGetallen);
-
-echo $abcGetallen[2];
+echo "<br>";
+echo $abcGetallen[0]    [0] . $abcGetallen[2];
+echo $abcGetallen[0]    [2] . $abcGetallen[4];
 ?>
-</ul>
 </body>
 </html>
