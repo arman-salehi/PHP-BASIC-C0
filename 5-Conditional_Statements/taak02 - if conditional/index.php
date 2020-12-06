da60022d0cff97d8f84393b35fe19a341639ee70
Arman Salehi <arman.salehi@student.rocva.nl>
2020-12-06 15:41:13 +0100

    if

---
+++ 5-Conditional_Statements/taak02 - if conditional/index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+$test1 = 10;
+$test2 = 6;
+if($test1 == $test2) {
+    echo "gelijk";
+}
+?>
+<br>
+<?php
+$test1 = 10;
+$test2 = 6;
+if($test1 != $test2) {
+    echo "ongelijk";
+}
+?>
+<br>
+<?php
+$test1 = 10;
+$test2 = 6;
+if($test1 === $test2) {
+    echo "identiek";
+}
+?>
+<br>
+<?php
+$test1 = 10;
+$test2 = 6;
+if($test1 > $test2) {
+    echo "test1 is groter dan test2";
+}
+?>
+<br>
+<?php
+$test1 = 10;
+$test2 = 6;
+if($test1 < $test2) {
+    echo "test1 is kleiner dan test2";
+}
+?>
+<br>
+<?php
+$test1 = 10;
+$test2 = 6;
+if($test1 >= $test2) {
+    echo "test2 is groter of gelijk aan test1";
+}
+?>
+<br>
+<?php
+$test1 = 10;
+$test2 = 6;
+if($test1 <= $test2) {
+    echo "test1 is kleiner of gelijk aan test2";
+}
+?>
+<br>
+</body>
+</html>
