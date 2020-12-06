6789d108ca1758f1a876e40ce7526d517801916c
Arman Salehi <arman.salehi@student.rocva.nl>
2020-12-06 11:47:31 +0100

    rekenen in php

---
+++ 4-Arithmetics/taak01 - Arithmetic Operators/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE HTML>
+<html>
+<body>
+<?php
+$Getallen1 = 12;
+$Getallen2 = 14;
+echo($Getallen1 + $Getallen2);
+?>
+<br>
+<?php
+$Getallen1 = 12;
+$Getallen2 = 14;
+echo($Getallen1 - $Getallen2);
+?>
+<br>
+<?php
+$Getallen1 = 12;
+$Getallen2 = 14;
+echo($Getallen1 * $Getallen2);
+?>
+<br>
+<?php
+$Getallen1 = 12;
+$Getallen2 = 14;
+echo($Getallen1 / $Getallen2)
+?>
+</body>
+</html>
