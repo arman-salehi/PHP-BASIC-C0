91102267cecd888144ecd874278c83cb3ed59c4d
Arman Salehi <arman.salehi@student.rocva.nl>
2020-12-06 11:30:01 +0100

    array sort

---
+++ 3-Arrays/taak05 - Array sorteren/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html>
+<body>
+<?php
+$getallen =  [676,5345,78657,3,46,21,47];
+arsort($getallen);
+print_r($getallen);
+
+$getallen =  [676,5345,78657,3,46,21,47];
+asort($getallen);
+print_r($getallen);
+?>
+</body>
+</html>
