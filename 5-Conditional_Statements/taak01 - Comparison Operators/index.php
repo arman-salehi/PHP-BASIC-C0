49f0fd76b91c0df7c3167726094c64526dce4f87
Arman Salehi <arman.salehi@student.rocva.nl>
2020-12-06 15:18:44 +0100

    true or false

---
+++ 5-Conditional_Statements/taak01 - Comparison Operators/index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+$test1 = 12;
+$test2 = 'lala';
+var_dump($test1 == $test2);
+?>
+<br>
+<?php
+$test1 = 12;
+$test2 = 'lala';
+var_dump($test1 === $test2);
+?>
+<br>
+<?php
+$test1 = 12;
+$test2 = 'lala';
+var_dump($test1 != $test2);
+?>
+<br>
+<?php
+$test1 = 12;
+$test2 = 'lala';
+var_dump($test1 > $test2);
+?>
+<br>
+<?php
+$test1 = 12;
+$test2 = 'lala';
+var_dump($test1 < $test2);
+?>
+<br>
+<?php
+$test1 = 12;
+$test2 = 'lala';
+var_dump($test1 <> $test2);
+?>
+<br>
+<?php
+$test1 = 12;
+$test2 = 'lala';
+var_dump($test1 >= $test2);
+?>
+<br>
+<?php
+$test1 = 12;
+$test2 = 'lala';
+var_dump($test1 <= $test2);
+?>
+<br>
+</body>
+</html>
