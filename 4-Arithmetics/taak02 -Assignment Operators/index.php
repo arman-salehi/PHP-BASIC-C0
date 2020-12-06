fab9ca4aac1a72d149a7ac270501e290d2856a8a
Arman Salehi <arman.salehi@student.rocva.nl>
2020-12-06 12:23:34 +0100

    -- en ++

---
+++ 4-Arithmetics/taak02 -Assignment Operators/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+$Getallen = 13;
+$Getallen += 12;
+echo "$Getallen <br>";
+
+$Getallen1 = 10;
+$Getallen1 -= 20;
+echo "$Getallen1 <br>";
+
+$Getallen2 = 10;
+$Getallen2 *= 20;
+echo "$Getallen2 <br>";
+
+$Getallen3 = 10;
+$Getallen3 /= 20;
+echo "$Getallen3 <br>";
+?>
+</body>
+</html>
---
+++ 4-Arithmetics/taak03 - Increment-operators/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+$Getallen = 10;
+echo ++$Getallen;
+?>
+<br>
+<?php
+$Getallen = 10;
+echo --$Getallen;
+?>
+</body>
+</html>
