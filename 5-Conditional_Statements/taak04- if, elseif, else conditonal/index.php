ddd5c202c851d1e37934c0c942524ae37ce1191e
Arman Salehi <arman.salehi@student.rocva.nl>
2020-12-08 13:23:49 +0100

    if elseif else

---
+++ 5-Conditional_Statements/taak04- if, elseif, else conditonal/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+$prod_besteld = 34;
+
+if($prod_besteld < 10) {
+    $prijs = 1.50;
+} elseif($prod_besteld < 20) {
+    $prijs = 1.25;
+} else {
+    $prijs = 1.00;
+}
+echo $prijs;
+?>
+<br>
+<?php
+$prod_besteld = 19;
+
+if($prod_besteld < 10) {
+    $prijs = 1.50;
+} elseif($prod_besteld < 20) {
+    $prijs = 1.25;
+} else {
+    $prijs = 1.00;
+}
+echo $prijs;
+?>
+<br>
+<?php
+$prod_besteld = 2;
+
+if($prod_besteld < 10) {
+    $prijs = 1.50;
+} elseif($prod_besteld < 20) {
+    $prijs = 1.25;
+} else {
+    $prijs = 1.00;
+}
+echo $prijs;
+?>
+</body>
+</html>
