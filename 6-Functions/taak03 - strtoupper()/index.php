3826bddb30a0c102e7c317b484dfc48091bdb219
Arman Salehi <arman.salehi@student.rocva.nl>
2020-12-10 13:10:02 +0100

    strtouper()

---
+++ 6-Functions/taak03 - strtoupper()/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+function telAlleWoorden(){
+    $var1 = 'Mijn blauwe auto is een mooie snelle wagen';
+    echo strlen($var1);
+}
+
+telAlleWoorden();
+?>
+</body>
+</html>
