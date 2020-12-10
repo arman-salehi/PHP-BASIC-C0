975b2b46a74cfa692f609a9ff6886b53055c062b
Arman Salehi <arman.salehi@student.rocva.nl>
2020-12-10 12:30:48 +0100

    function declaration

---
+++ 6-Functions/taak01 - function declaration/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+function zetTweeWoordenAanElkaar(){
+    $woordje = 'Hallo ';
+    $zinnetje = 'mooie blauwe planeet!';
+
+    $heleZin = ($woordje . $zinnetje);
+    echo $heleZin;
+}
+
+zetTweeWoordenAanElkaar();
+?>
+</body>
+</html>
